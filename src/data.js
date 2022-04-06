1aaf64bb26f83c52285cc88653dd5df6861ef656
Irsyad <irsyadrbbn19@gmail.com>
2022-04-06 20:23:55 +0700

    Create landing page

---
+++ src/data.js
@@ -0,0 +1,77 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "./assets/mens-2.png",
+        title: "SHIRT SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET 30% OFF FROM NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "./assets/mens-1.png",
+        title: "FORMAL SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET 30% OFF FROM NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 2,
+        img: "./assets/mens-3.png",
+        title: "CASUAL SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET 30% OFF FROM NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+
+];
+
+export const categories = [
+    {
+        id:1,
+        img: "./assets/categories-3.jpeg",
+        title: "SHIRT STYLE",
+    },
+    {
+        id:2,
+        img: "./assets/categories-1.jpeg",
+        title: "FORMAL STYLE",
+    },
+    {
+        id:3,
+        img: "./assets/categories-2.jpeg",
+        title: "JACKETS",
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "./assets/Tshirt-White.jpg",
+    },
+    {
+        id: 2,
+        img: "./assets/Tshirt-Black.jpg",
+    },
+    {
+        id: 3,
+        img: "./assets/KP-white.jpg",
+    },
+    {
+        id: 4,
+        img: "./assets/KP-black.jpg",
+    },
+    {
+        id: 5,
+        img: "./assets/Kemeja-White.jpg",
+    },
+    {
+        id: 6,
+        img: "./assets/Kemeja-Black.jpg",
+    },
+    {
+        id: 7,
+        img: "./assets/Hoodie-Cream.jpg",
+    },
+    {
+        id: 8,
+        img: "./assets/Hoodie-Blue.jpg",
+    },
+];
