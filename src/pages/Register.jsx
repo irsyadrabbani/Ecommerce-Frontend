34fa519f8dad4edcba5a46c4b76375e597d324ac
Irsyad <irsyadrbbn19@gmail.com>
2022-04-08 22:04:34 +0700

    Make Login&Register page mobile responsive

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { mobile } from '../responsive'
 
 const Container = styled.div`
     width: 100vw;
@@ -13,6 +14,7 @@ const Wrapper = styled.div`
     width: 40%;
     padding: 20px;
     background-color: white;
+    ${mobile({ width: "75%"})}
 `
 const Form = styled.form`
     display: flex;
@@ -39,6 +41,7 @@ const Button = styled.button`
     background-color: teal;
     color: white;
     cursor: pointer;
+    ${mobile({ width: "50%"})}
 `
 
 const Register = () => {
