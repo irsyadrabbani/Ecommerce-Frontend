93fc569b24d2fb687491c4af3ed039be1873e352
Irsyad <irsyadrbbn19@gmail.com>
2022-04-08 23:06:35 +0700

    Make Product page mobile responsive

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,12 +5,14 @@ import Announcement from '../components/Announcement'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Newsletter from '../components/Newsletter'
+import { mobile } from '../responsive'
 
 const Container = styled.div``
 
 const Wrapper = styled.div`
     padding: 5px;
     display: flex;
+    ${mobile({ padding: "10px", flexDirection:"column"})}
 `
 const ImgContainer = styled.div`
     flex: 1;    
@@ -20,11 +22,13 @@ const Image = styled.img`
     width: 100%;
     height: 90vh;
     object-fit: cover;
+    ${mobile({ height: "70vh"})}
 `
 
 const InfoContainer = styled.div`
     flex: 1;
     padding: 0px 50px;
+    ${mobile({ padding: "10px"})}
 `
 
 const Title = styled.h1`
@@ -45,6 +49,8 @@ const FilterContainer = styled.div`
     margin: 30px 0px;
     display: flex;
     justify-content: space-between;
+    ${mobile({ width: "100%"})}
+
 `
 
 const Filter = styled.div`
@@ -79,6 +85,7 @@ const AddContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    ${mobile({ width: "100%"})}
 `
 const AmountContainer = styled.div`
     display: flex;
